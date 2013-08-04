79999687e57a5ec110ce49a127fc90a79006506b
seagsm_trash@mail.ru <seagsm_trash@mail.ru@2cd08345-79ab-67d9-668f-5cbaafc01595>
2013-08-04 20:11:22 +0000

    Just work! Board I2S in slave mode, codec in master mode. 96000.

--- trunk/ digital-metal-detector --username seagsm_trash@mail.ru/src/WM8731/WM8731.c
+++ trunk/ digital-metal-detector --username seagsm_trash@mail.ru/src/WM8731/WM8731.c
@@ -23,7 +23,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "WM8731.h"
 
-#define MASTER_ON 0
+#define MASTER_ON 1
 
 #if 1
 /*******************************************************************************
@@ -226,11 +226,16 @@ void WM8731_Init(void)
 
 #if MASTER_ON
    codec_send( SAMPLING_CONTROL | NORMAL_MODE | BOSR_NORMAL_256FS |(0x07)<< 2 );/* 96000 sampling rate.*/
+   //codec_send( SAMPLING_CONTROL | NORMAL_MODE | BOSR_NORMAL_256FS |(2)<< 2 );/* 8000 sampling rate.*/
 #else
    codec_send( SAMPLING_CONTROL | NORMAL_MODE | BOSR_NORMAL_256FS );
 #endif
-   codec_send( ACTIVE_CONTROL | ACTIVE );
+   //codec_send( ACTIVE_CONTROL | ACTIVE );
+}
 
+void WM8731_Active(void)
+{
+   codec_send( ACTIVE_CONTROL | ACTIVE );
 }
 
 /*******************************************************************************
